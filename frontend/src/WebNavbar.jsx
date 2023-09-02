068edc7b1bd321758d667f423e691083ba28ab4b
Nattaphon <nattaphonnuayen@gmail.com>
2023-09-02 21:36:24 +0700

    update scss

--- frontend/src/WebNavbar.jsx
+++ frontend/src/WebNavbar.jsx
@@ -2,15 +2,16 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import "./WebNavbar.css";
 import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
+
 export function WebNavBar() {
   return (
-    <Navbar expand="lg" className="justify-content-between" bg="secondary" variant="light">
-      <Container>
-        <Navbar.Brand href="#home">Rsseact-Bootstrap</Navbar.Brand>
-        <Nav className="me-auto" color="red">
-           <Nav.Link className="60" as = {Link} to="/">Home</Nav.Link>
-          <Nav.Link as = {Link} to="/passwordchecker">Password Checker</Nav.Link>
-          <Nav.Link as = {Link} to="/member">Members</Nav.Link> 
+    <Navbar expand="lg" className="justify-content-between" bg="secondary">
+      <Container className="mx-5">
+        <Navbar.Brand as = {Link} to="/" className="text-primary"> <h1>Computation</h1></Navbar.Brand>
+        <Nav className="me-auto" >
+           <Nav.Link  as = {Link} to="/" className="text-primary px-4">Home</Nav.Link>
+          <Nav.Link as = {Link} to="/passwordchecker" className="text-primary px-4">Password Checker</Nav.Link>
+          <Nav.Link as = {Link} to="/member" className="text-primary px-4">Members</Nav.Link> 
         </Nav>
       </Container>
     </Navbar>
