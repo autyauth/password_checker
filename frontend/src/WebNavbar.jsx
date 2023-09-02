b081de544f8677b29fe064b70329a82d4d23f7b2
Nattaphon <nattaphonnuayen@gmail.com>
2023-09-02 17:50:36 +0700

    nb 1.1

--- frontend/src/WebNavbar.jsx
+++ frontend/src/WebNavbar.jsx
@@ -4,17 +4,14 @@ import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 export function WebNavBar() {
   return (
-    <Navbar expand="lg" className="justify-content-between">
+    <Navbar expand="lg" className="justify-content-between" bg="secondary" variant="light">
       <Container>
         <Navbar.Brand href="#home">Rsseact-Bootstrap</Navbar.Brand>
-        <Nav className="me-auto">
-           <Nav.Link as = {Link} to="/">Home</Nav.Link>
+        <Nav className="me-auto" color="red">
+           <Nav.Link className="60" as = {Link} to="/">Home</Nav.Link>
           <Nav.Link as = {Link} to="/passwordchecker">Password Checker</Nav.Link>
           <Nav.Link as = {Link} to="/member">Members</Nav.Link> 
         </Nav>
-        <LinkContainer to ="">
-          <a>Hi</a>
-        </LinkContainer>
       </Container>
     </Navbar>
   );
