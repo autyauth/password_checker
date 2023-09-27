fe09a465e1413c87df560f698e974e3cd9d418b1
Thanakan Kanphairor <taninwlol001@gmail.com>
2023-09-28 01:34:42 +0700

    Revert "clear"
    This reverts commit 830c13fd92fefe925dba80fc517932fc2ef2877d.

--- frontend/src/TimeResult.jsx
+++ frontend/src/TimeResult.jsx
@@ -1,15 +1,9 @@
 export function TimeResult(){
     return(
         <div className="text-center timeResult">
-<<<<<<< Updated upstream
             <h6 className="text-secondary">This password take</h6>
             <h3 className="text-success ">2 Million Year</h3>
             <h6 className="text-secondary">to bruteforce.</h6>
-=======
-            <p className="text-primary">this password take</p>
-            <h3 className="text-success ">5 Million Year</h3>
-            <p className="text-primary">to bruteforce.</p>
->>>>>>> Stashed changes
         </div>
     )
 }
